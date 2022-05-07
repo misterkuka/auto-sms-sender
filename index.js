64d6fc6eb1fdb66e0c1fd7dd65da4cb41e8241c0
Igor Bojczuk <igorbojczuk@Igors-MacBook-Pro.local>
2022-05-07 16:11:20 +0200

    noice

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/', function (req, res) {
 
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
    var host = server.address().address
    var port = server.address().port
    
